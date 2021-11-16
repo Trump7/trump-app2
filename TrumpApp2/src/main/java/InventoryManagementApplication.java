f5b08c056e4dbb653f0adbcbbfa4c555fcfd896f
Trump7 <trump@knights.ucf.edu>
2021-11-16 03:56:20 -0500

    project upload

---
+++ TrumpApp2/src/main/java/InventoryManagementApplication.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 Matthew Trump
+ */
+
+public class InventoryManagementApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
