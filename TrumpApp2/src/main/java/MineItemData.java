f83575d2346b5a08fa7a2c563a896456df446109
Trump7 <trump@knights.ucf.edu>
2021-11-18 20:54:12 -0500

    Item Data Holder setup

--- TrumpApp2/src/main/java/MineItemData.java
+++ TrumpApp2/src/main/java/MineItemData.java
@@ -1,2 +1,36 @@
 public class MineItemData {
+
+    private String name;
+    private String serial;
+    private float value;
+
+    public MineItemData(String name, Float value, String serial) {
+        this.setName(name);
+        this.setSerial(serial);
+        this.setValue(value);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public float getValue(){
+        return value;
+    }
+
+    public void setValue(Float value){
+        this.value = value;
+    }
+
+    public String getSerial(){
+        return serial;
+    }
+
+    public void setSerial(String serial){
+        this.serial = serial;
+    }
 }
