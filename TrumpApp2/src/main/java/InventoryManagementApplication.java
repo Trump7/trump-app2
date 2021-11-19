8a762e6d4d3a029ec2a60e7ae2c8fe26a2f37ad3
Trump7 <trump@knights.ucf.edu>
2021-11-18 20:24:54 -0500

    Java Files with some plan

--- TrumpApp2/src/main/java/InventoryManagementApplication.java
+++ TrumpApp2/src/main/java/InventoryManagementApplication.java
@@ -2,10 +2,30 @@
  *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
  *  Copyright 2021 Matthew Trump
  */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class InventoryManagementApplication {
+import java.io.IOException;
+import java.util.Objects;
 
-    public static void main(String[] args) {
+public class InventoryManagementApplication extends Application{
 
-    }
+        public void start(Stage stage) throws IOException {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("minecraftApp.fxml")));
+
+            Scene scene = new Scene(root);
+
+            stage.setResizable(false);
+            //Code for the application displaying
+            stage.setTitle("Minecraft Inventory Manager");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
 }
